df82e70c242f81b5cdd7ad1462b67dded466129d
Torremolinos <adrian_2054@hotmail.com>
2024-03-30 03:18:59 +0100

    Trabajando con twig, pasando del controlador los valores de track

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,24 +5,36 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: PB and Jams');
+        $tracks = [
+            ['song' => 'Gansta\'s Paradise', 'artist'=> 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist'=> 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist'=>' Seal'],
+            ['song' => 'On Bended Knee', 'artist'=>'Boyz II Men'],
+            ['song' => 'Fantasy ','artist'=>' Mariah Carey'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [ // render() method is inherited from AbstractController Y bueno tambien el nombre vinyl debe coincidir con el controlador y que homepage debe coincidir a dodne deberia ir
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
-
-
     #[Route('/browse/{slug}')]
     public function browse(string $slug = null): Response
     {
         if ($slug) {
-            $tittle ='Genre : ' . (str_replace('-', ' ', $slug));
+            $title = 'Genre: ' . u(str_replace('-', ' ', $slug))->title(true);
         } else {
-            $tittle = 'All Genres';
+            $title = 'All Genres';
         }
-        return new Response($tittle);
+        return new Response($title);
+        //return new Response('Breakup vinyl? Angsty 90s rock? Browse the collection!');
     }
 }
