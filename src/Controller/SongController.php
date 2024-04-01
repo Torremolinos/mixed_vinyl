e25459bcc251c62b990f1f3170c9ae0ebb95d27b
Torremolinos <adrian_2054@hotmail.com>
2024-04-01 03:43:50 +0200

    in case u need render get this commit

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,8 +11,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SongController extends AbstractController
 {
-    #[Route('/api/songs/{id}')]
-    public function getSong($id): Response
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
         // TODO query the database
         $song = [
@@ -19,6 +20,13 @@ class SongController extends AbstractController
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+
+        $logger->info('Returning Api Respones for son {song}');
+        [
+
+            'song' => $id,
+        ];
+
         // return new JsonResponse($song);  el de abajo es un shortcut un atajo vaya.
         return $this->json($song);
     }
