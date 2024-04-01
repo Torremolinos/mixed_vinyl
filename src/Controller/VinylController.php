111f6548dde96b09c3211e2ffd6271b625d7705c
Torremolinos <adrian_2054@hotmail.com>
2024-04-01 18:30:19 +0200

    Setting up webpack Encore

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,14 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
-
+ 
 use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function homepage(Environment $twig): Response
+    public function homepage(): Response
     {
         $tracks = [
             ['song' => 'Gansta\'s Paradise', 'artist'=> 'Coolio'],
@@ -23,12 +22,11 @@ class VinylController extends AbstractController
             ['song' => 'Fantasy ','artist'=>' Mariah Carey'],
         ];
         dump($tracks); //mejor sale abajo en la consola
-        $html= $twig->render('vinyl/homepage.html.twig', [ // render() method is inherited from AbstractController Y bueno tambien el nombre vinyl debe coincidir con el controlador y que homepage debe coincidir a dodne deberia ir
+       return $this->render('vinyl/homepage.html.twig', [ // render() method is inherited from AbstractController Y bueno tambien el nombre vinyl debe coincidir con el controlador y que homepage debe coincidir a dodne deberia ir
             'title' => 'PB & Jams',
             'tracks' => $tracks,
-        ]);
+        ]); 
 
-        return new Response($html);
     }
     #[Route('/browse/{slug}', name: 'app_browse')]
     public function browse(string $slug = null): Response
